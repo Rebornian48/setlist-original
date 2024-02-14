35efe005b8cc8b0461c78b62fd80e3ee5cf6ee7b
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2024-02-14 23:52:25 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
 const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-const firstDate = new Date(2021, 12, 18);
-const secondDate = new Date(2022, 9, 8);
+const firstDate = new Date(2021, 11, 18);
+const secondDate = new Date(2022, 8, 8);
 const nowDate = new Date();
+
+var id1Date = new Intl.DateTimeFormat("id-ID", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+}).format(firstDate);
+
+var id2Date = new Intl.DateTimeFormat("id-ID", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+}).format(secondDate);
 
 const diffDays1 = Math.round(Math.abs((nowDate - firstDate) / oneDay));
 const diffDays2 = Math.round(Math.abs((nowDate - secondDate) / oneDay));
 
+document.getElementById('tglsatu').textContent = id1Date;
+document.getElementById('tgldua').textContent = id2Date;
 document.getElementById('satu').textContent = diffDays1 + ' hari';
 document.getElementById('dua').textContent = diffDays2 + ' hari';
